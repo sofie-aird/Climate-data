ca791df887e30dfd23b7f109de1018bcedfbde11
Tia Newhall <newhall@cs.swarthmore.edu>
2024-04-01 20:12:43 -0400

    Initial commit.

---
+++ climate2.py
@@ -0,0 +1,38 @@
+"""
+TODO: add a top-level comment
+
+climate2.py
+"""
+
+from cs21_country import *
+
+# you can import some of our lab8 solution functions to use like this:
+# from cs21S24_lab08lib import binary_search
+
+def main():
+
+    # NOTE: you can test your code on a smaller file (small.csv) by
+    #       uncommenting this line, and commenting out the one that
+    #       sets filename to climatewatchdata.csv
+    #filename = "small.csv"
+    filename = "/usr/local/doc/climatewatchdata.csv"
+
+    print("Debug: using file %s" %(filename))
+    
+
+###########################################
+# TODO: Add your functions, including copying over some from your 
+#       lab 8 solution, here:
+
+
+
+
+
+
+
+
+
+
+
+############
+main()
